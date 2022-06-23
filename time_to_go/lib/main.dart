ec093ee027879f9a820762adc56709ad48986654
husainunwalla <mr.husainunwalla@gmail.com>
2022-06-23 11:15:59 +0530

    UI Chnages

--- time_to_go/lib/main.dart
+++ time_to_go/lib/main.dart
@@ -1,7 +1,9 @@
+import 'dart:developer';
 import 'dart:ui';
-
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
+import 'package:intl/intl.dart';
+import 'dart:async';
 
 void main() {
   runApp(const MyApp());
@@ -31,14 +33,23 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int daysBetween(DateTime from, DateTime to) {
-    from = DateTime(from.year, from.month, from.day);
-    to = DateTime(to.year, to.month, to.day);
-    return (to.difference(from).inHours / 24).round();
-  }
-
-  final now = DateTime.now();
+  var now = DateTime.now();
   final flight = DateTime.parse('2022-09-02 23:40:00');
+  var time = 'Hello';
+
+  @override
+  void initState() {
+    super.initState();
+
+    // defines a timer
+    Timer everySecond = Timer.periodic(const Duration(seconds: 1), (Timer t) {
+      setState(() {
+        now = DateTime.now();
+        time =
+            '${flight.difference(now).inDays}D ${flight.difference(now).inHours % 24}H ${flight.difference(now).inMinutes % 60}M ${flight.difference(now).inSeconds % 60}S';
+      });
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -47,64 +58,84 @@ class _MyHomePageState extends State<MyHomePage> {
       ConstrainedBox(
         constraints: const BoxConstraints.expand(),
         child: Image.asset(
-          'assets/golden_gate.png',
+          'assets/waves.jpg',
           fit: BoxFit.cover,
           alignment: Alignment.topLeft,
         ),
       ),
       Center(
-          child: ClipRect(
-              child: BackdropFilter(
-                  filter: ImageFilter.blur(sigmaX: 10.0, sigmaY: 10.0),
-                  child: Container(
-                      width: 375,
-                      height: 375,
-                      decoration: BoxDecoration(
-                          color: Colors.grey.shade200.withOpacity(0.5)),
-                      child: Center(
-                        child: Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              Image.asset(
-                                'assets/plane.gif',
-                                width: 150,
-                                height: 150,
-                              ),
-                              Row(
-                                mainAxisAlignment:
-                                    MainAxisAlignment.spaceEvenly,
-                                children: [
-                                  Text(
-                                    '${flight.difference(now).inDays}D',
-                                    style: TextStyle(
-                                        color: Colors.grey[800],
-                                        fontWeight: FontWeight.w900,
-                                        fontStyle: FontStyle.italic,
-                                        fontFamily: 'Open Sans',
-                                        fontSize: 40),
+          child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+            Container(
+                decoration: const BoxDecoration(
+                  //Here goes the same radius, u can put into a var or function
+                  borderRadius:
+                      BorderRadius.only(bottomLeft: Radius.circular(20)),
+                  boxShadow: [
+                    BoxShadow(
+                      color: Color.fromARGB(0, 0, 0, 0),
+                      spreadRadius: 20,
+                      blurRadius: 10,
+                    ),
+                  ],
+                ),
+                child: Padding(
+                    padding: const EdgeInsets.all(15),
+                    child: ClipRRect(
+                        borderRadius:
+                            const BorderRadius.all(Radius.circular(20)),
+                        child: BackdropFilter(
+                            filter:
+                                ImageFilter.blur(sigmaX: 100.0, sigmaY: 100.0),
+                            child: Padding(
+                                padding: const EdgeInsets.all(15),
+                                child: Center(
+                                  child: Column(
+                                    mainAxisAlignment: MainAxisAlignment.center,
+                                    crossAxisAlignment:
+                                        CrossAxisAlignment.center,
+                                    children: [
+                                      const Text(
+                                        'Friday\nSEPTEMBER 2, 2022\n11:40 PM',
+                                        style: TextStyle(
+                                            color: Color.fromARGB(
+                                                200, 255, 255, 255),
+                                            fontWeight: FontWeight.w900,
+                                            fontStyle: FontStyle.normal,
+                                            fontFamily: 'Open Sans',
+                                            fontSize: 33),
+                                        textAlign: TextAlign.left,
+                                      ),
+                                      Image.asset(
+                                        'assets/plane.gif',
+                                        width: 150,
+                                        height: 150,
+                                      ),
+                                      const Text(
+                                        'Take-Off In',
+                                        style: TextStyle(
+                                            color: Color.fromARGB(
+                                                199, 255, 164, 164),
+                                            fontWeight: FontWeight.w900,
+                                            fontStyle: FontStyle.normal,
+                                            fontFamily: 'Open Sans',
+                                            fontSize: 30),
+                                      ),
+                                      Text(
+                                        time,
+                                        style: const TextStyle(
+                                            color: Color.fromARGB(
+                                                200, 255, 255, 255),
+                                            fontWeight: FontWeight.w900,
+                                            fontStyle: FontStyle.normal,
+                                            fontFamily: 'Open Sans',
+                                            fontSize: 30),
+                                      ),
+                                    ],
                                   ),
-                                  Text(
-                                    '${flight.difference(now).inHours % 24}H',
-                                    style: TextStyle(
-                                        color: Colors.grey[800],
-                                        fontWeight: FontWeight.w900,
-                                        fontStyle: FontStyle.italic,
-                                        fontFamily: 'Open Sans',
-                                        fontSize: 40),
-                                  ),
-                                  Text(
-                                    '${flight.difference(now).inMinutes % 60}M',
-                                    style: TextStyle(
-                                        color: Colors.grey[800],
-                                        fontWeight: FontWeight.w900,
-                                        fontStyle: FontStyle.italic,
-                                        fontFamily: 'Open Sans',
-                                        fontSize: 40),
-                                  )
-                                ],
-                              )
-                            ]),
-                      )))))
+                                ))))))
+          ]))
     ]));
   }
 }
